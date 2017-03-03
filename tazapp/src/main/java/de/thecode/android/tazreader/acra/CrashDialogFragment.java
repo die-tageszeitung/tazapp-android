a74e61e65e7926b76633194c85a40d9cca72b68d
theCode AG, Mate <tcgitbot@users.noreply.github.com>
2017-03-04 00:52:15 +0100

    CrashDialog as fragment

---
+++ tazapp/src/main/java/de/thecode/android/tazreader/acra/CrashDialogFragment.java
@@ -0,0 +1,126 @@
+package de.thecode.android.tazreader.acra;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import de.thecode.android.tazreader.R;
+
+import org.acra.ACRAConstants;
+import org.acra.config.ACRAConfiguration;
+
+/**
+ * Created by Mate on 03.03.2017.
+ */
+public class CrashDialogFragment extends DialogFragment {
+
+    private static final String ARG_CONFIG = "config";
+    private EditText editCommentText;
+    private EditText editEmailText;
+
+    public static CrashDialogFragment newInstance(ACRAConfiguration config) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CONFIG, config);
+        CrashDialogFragment fragment = new CrashDialogFragment();
+        fragment.setArguments(args);
+        fragment.setCancelable(false);
+        return fragment;
+    }
+
+    private CrashActivtyCallback callback;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final ACRAConfiguration config = (ACRAConfiguration) getArguments().getSerializable(ARG_CONFIG);
+        assert config != null;
+
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+
+        final int titleResourceId = config.resDialogTitle();
+        if (titleResourceId != ACRAConstants.DEFAULT_RES_VALUE) {
+            alertDialogBuilder.setTitle(titleResourceId);
+        }
+        final int iconResourceId = config.resDialogIcon();
+        if (iconResourceId != ACRAConstants.DEFAULT_RES_VALUE) {
+            alertDialogBuilder.setIcon(iconResourceId);
+        }
+        alertDialogBuilder.setView(buildCustomView(config));
+
+        alertDialogBuilder.setPositiveButton(config.resDialogPositiveButtonText(), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                callback.onSend(editCommentText.getText()
+                                               .toString(), editEmailText.getText()
+                                                                         .toString(), false);
+            }
+        });
+        alertDialogBuilder.setNegativeButton(config.resDialogNegativeButtonText(), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                callback.onCancel();
+            }
+        });
+        alertDialogBuilder.setNeutralButton(R.string.crash_dialog_always_accept, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                callback.onSend(editCommentText.getText()
+                                               .toString(), editEmailText.getText()
+                                                                         .toString(), true);
+            }
+        });
+        return alertDialogBuilder.create();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        callback = (CrashActivtyCallback) getActivity();
+    }
+
+    private View buildCustomView(ACRAConfiguration config) {
+        View root = LayoutInflater.from(getContext())
+                                  .inflate(R.layout.crash_dialog, null);
+
+        TextView dialogText = (TextView) root.findViewById(R.id.dialogText);
+        if (config.resDialogText() != ACRAConstants.DEFAULT_RES_VALUE) {
+            dialogText.setText(config.resDialogText());
+        } else {
+            dialogText.setVisibility(View.GONE);
+        }
+
+        TextView commentText = (TextView) root.findViewById(R.id.commentText);
+        editCommentText = (EditText) root.findViewById(R.id.commentEditText);
+        if (config.resDialogCommentPrompt() != ACRAConstants.DEFAULT_RES_VALUE) {
+            commentText.setText(config.resDialogCommentPrompt());
+        } else {
+            commentText.setVisibility(View.GONE);
+            editCommentText.setVisibility(View.GONE);
+        }
+
+        TextView emailText = (TextView) root.findViewById(R.id.emailText);
+        editEmailText = (EditText) root.findViewById(R.id.emailEditText);
+        if (config.resDialogEmailPrompt() != ACRAConstants.DEFAULT_RES_VALUE) {
+            commentText.setText(config.resDialogEmailPrompt());
+        } else {
+            emailText.setVisibility(View.GONE);
+            editEmailText.setVisibility(View.GONE);
+        }
+
+        return root;
+    }
+
+    protected interface CrashActivtyCallback {
+        void onSend(String comment, String userEmail, boolean always);
+
+        void onCancel();
+    }
+
+}
--- tazapp/src/main/java/de/thecode/android/tazreader/acra/NewTazCrashDialog.java
+++ tazapp/src/main/java/de/thecode/android/tazreader/acra/NewTazCrashDialog.java
@@ -1,118 +1,56 @@
 package de.thecode.android.tazreader.acra;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.text.TextUtils;
 
-import de.thecode.android.tazreader.R;
-
-import org.acra.ACRAConstants;
-import org.acra.config.ACRAConfiguration;
+import org.acra.ACRA;
 import org.acra.dialog.BaseCrashReportDialog;
+import org.acra.prefs.PrefUtils;
+import org.acra.prefs.SharedPreferencesFactory;
 
 /**
  * Created by mate on 03.03.2017.
  */
 
-public class NewTazCrashDialog extends BaseCrashReportDialog {
+public class NewTazCrashDialog extends BaseCrashReportDialog implements CrashDialogFragment.CrashActivtyCallback {
 
     private static final String DIALOG_TAG = "CRASHDIALOG";
+    private SharedPreferencesFactory sharedPreferencesFactory;
 
     @Override
     protected void init(@Nullable Bundle savedInstanceState) {
+        sharedPreferencesFactory = new SharedPreferencesFactory(getApplicationContext(), getConfig());
         if (getSupportFragmentManager().findFragmentByTag(DIALOG_TAG) == null) {
             CrashDialogFragment.newInstance(getConfig())
                                .show(getSupportFragmentManager(), DIALOG_TAG);
         }
     }
 
-    public static class CrashDialogFragment extends DialogFragment {
-
-        private static final int PADDING = 10;
-
-        private static final String STATE_EMAIL   = "email";
-        private static final String STATE_COMMENT = "comment";
-
-        private static final String ARG_CONFIG = "config";
-
-        public static CrashDialogFragment newInstance(ACRAConfiguration config) {
-            Bundle args = new Bundle();
-            args.putSerializable(ARG_CONFIG, config);
-            CrashDialogFragment fragment = new CrashDialogFragment();
-            fragment.setArguments(args);
-            fragment.setCancelable(false);
-            return fragment;
+    @Override
+    public void onSend(String comment, String userEmail, boolean always) {
+        final SharedPreferences prefs = sharedPreferencesFactory.create();
+        final SharedPreferences.Editor prefEditor = prefs.edit();
+        if (always) prefEditor.putBoolean(ACRA.PREF_ALWAYS_ACCEPT, true);
+        if (TextUtils.isEmpty(comment)) {
+            comment = "";
         }
-
-        @NonNull
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            final ACRAConfiguration config = (ACRAConfiguration) getArguments().getSerializable(ARG_CONFIG);
-            assert config != null;
-
-            final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
-
-            final int titleResourceId = config.resDialogTitle();
-            if (titleResourceId != ACRAConstants.DEFAULT_RES_VALUE) {
-                alertDialogBuilder.setTitle(titleResourceId);
-            }
-            final int iconResourceId = config.resDialogIcon();
-            if (iconResourceId != ACRAConstants.DEFAULT_RES_VALUE) {
-                alertDialogBuilder.setIcon(iconResourceId);
-            }
-            alertDialogBuilder.setView(buildCustomView(savedInstanceState,config));
-
-            alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-            alertDialogBuilder.setNegativeButton("ABBRECHEN", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-            alertDialogBuilder.setNeutralButton("IMMER SENDEN", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-            return alertDialogBuilder.create();
+        if (!TextUtils.isEmpty(userEmail)) {
+            prefEditor.putString(ACRA.PREF_USER_EMAIL_ADDRESS, userEmail);
+        } else {
+            userEmail = "";
         }
-
-        private View buildCustomView(Bundle savedInstanceState, ACRAConfiguration config) {
-
-            View root = LayoutInflater.from(getContext()).inflate(R.layout.crash_dialog, null);
-
-            return root;
-        }
-
-        private void addView(LinearLayout layout, View viewToAdd, boolean topPadding) {
-            viewToAdd.setPadding(viewToAdd.getPaddingLeft(), topPadding ? dpToPx(PADDING) : viewToAdd.getPaddingTop(),
-                                 viewToAdd.getPaddingRight(), viewToAdd.getPaddingBottom());
-            layout.addView(viewToAdd);
-        }
-
-        private int dpToPx(int dp) {
-            return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getContext().getResources()
-                                                                                                .getDisplayMetrics());
-        }
-
-        @Override
-        public void onSaveInstanceState(Bundle outState) {
-            super.onSaveInstanceState(outState);
-        }
+        PrefUtils.save(prefEditor);
+        sendCrash(comment, userEmail);
+        finish();
     }
+
+    @Override
+    public void onCancel() {
+        cancelReports();
+        finish();
+    }
+
 
 }
