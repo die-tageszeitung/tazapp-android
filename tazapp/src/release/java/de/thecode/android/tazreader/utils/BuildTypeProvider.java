a3fd70efd70b43f82580c5a06c8f44ecce8238f1
theCode AG, Mate <tcgitbot@users.noreply.github.com>
2017-08-10 15:31:08 +0200

    missing

---
+++ tazapp/src/release/java/de/thecode/android/tazreader/utils/BuildTypeProvider.java
@@ -0,0 +1,23 @@
+package de.thecode.android.tazreader.utils;
+
+import android.content.Context;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by mate on 10.05.2016.
+ */
+public class BuildTypeProvider {
+
+    public static void installStetho(Context context) {
+
+    }
+
+    public static void addStethoInterceptor(OkHttpClient.Builder builder) {
+    }
+
+    public static void addLoggingInterceptor(OkHttpClient.Builder builder) {
+
+    }
+
+}
