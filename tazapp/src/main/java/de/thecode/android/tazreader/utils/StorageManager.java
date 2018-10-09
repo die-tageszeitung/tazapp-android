74500e1caa097b21b6e181313ee4462b94dd6296
theCode AG, Mate <tcgitbot@users.noreply.github.com>
2018-10-09 10:36:59 +0200

    Added Pref,added MaterialDialogs

--- tazapp/src/main/java/de/thecode/android/tazreader/utils/StorageManager.java
+++ tazapp/src/main/java/de/thecode/android/tazreader/utils/StorageManager.java
@@ -2,10 +2,14 @@ package de.thecode.android.tazreader.utils;
 
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 
 import de.thecode.android.tazreader.data.FileCachePDFThumbHelper;
 import de.thecode.android.tazreader.data.Paper;
 import de.thecode.android.tazreader.data.Resource;
+import de.thecode.android.tazreader.data.TazSettings;
 import de.thecode.android.tazreader.secure.HashHelper;
 
 import org.apache.commons.io.FileUtils;
@@ -14,6 +18,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import timber.log.Timber;
 
@@ -24,23 +33,70 @@ public class StorageManager extends ContextWrapper {
     public static final String PAPER    = "paper";
     public static final String RESOURCE = "resource";
 
-    private static final String DOWNLOAD = "download";
-    private static final String IMPORT   = "import";
-    private static final String APPUPDATE   = "appUpdate";
-    private static final String LOG = "logs";
+    private static final String DOWNLOAD  = "download";
+    private static final String IMPORT    = "import";
+    private static final String APPUPDATE = "appUpdate";
+    private static final String LOG       = "logs";
+
 
 
     private static StorageManager instance;
+
+    private static class Volume {
+        private final boolean isRemovable;
+        private final boolean isEmulated;
+        private final String root;
+
+        public Volume(File file) {
+            root = file.getAbsolutePath();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                isRemovable = Environment.isExternalStorageRemovable(file);
+                isEmulated = Environment.isExternalStorageEmulated(file);
+            } else {
+                isRemovable = Environment.isExternalStorageRemovable();
+                isEmulated = Environment.isExternalStorageEmulated();
+            }
+        }
+
+        public File getFile() {
+            return new File(root);
+        }
+
+        public static List<Volume> getVolumes(Context context){
+            List<File> dirList = new ArrayList<>(Arrays.asList(ContextCompat.getExternalFilesDirs(context, null)));
+            List<Volume> result = new ArrayList<>();
+            for (File dir : dirList) {
+                result.add(new Volume(dir));
+            }
+            Collections.sort(result, new Comparator<Volume>() {
+                @Override
+                public int compare(Volume o1, Volume o2) {
+                    // -1 - less than, 1 - greater than, 0 - equal, all inversed for descending
+                    if (o1.isEmulated && !o2.isEmulated) return -1;
+                    if (o2.isEmulated && !o1.isEmulated) return 1;
+                    return 0;
+                }
+            });
+            return result;
+        }
+    }
 
     public static StorageManager getInstance(Context context) {
         if (instance == null) instance = new StorageManager(context.getApplicationContext());
         return instance;
     }
 
+    private final TazSettings settings;
+
     private StorageManager(Context context) {
         super(context);
+        settings = TazSettings.getInstance(context);
         createNoMediaFileInDir(getCache(null));
         createNoMediaFileInDir(get(null));
+
+        List<Volume> volumes = Volume.getVolumes(context);
+        Timber.i("Found dirs");
+
     }
 
     private void createNoMediaFileInDir(File dir) {
