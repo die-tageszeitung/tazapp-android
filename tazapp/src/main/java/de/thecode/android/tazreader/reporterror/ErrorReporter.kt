29f9dbda0be8350413ba309c58a45843aae02e86
theCode AG, Mate <tcgitbot@users.noreply.github.com>
2019-02-08 15:21:39 +0100

    get some more infos in error mail,version

--- tazapp/src/main/java/de/thecode/android/tazreader/reporterror/ErrorReporter.kt
+++ tazapp/src/main/java/de/thecode/android/tazreader/reporterror/ErrorReporter.kt
@@ -1,6 +1,8 @@
 package de.thecode.android.tazreader.reporterror
 
+import android.app.ActivityManager
 import android.content.Context
+import android.content.Context.ACTIVITY_SERVICE
 import android.content.Intent
 import android.os.Build
 import com.commonsware.cwac.provider.StreamProvider
@@ -8,9 +10,12 @@ import de.thecode.android.tazreader.BuildConfig
 import de.thecode.android.tazreader.R
 import de.thecode.android.tazreader.data.TazSettings
 import de.thecode.android.tazreader.sync.AccountHelper
+import de.thecode.android.tazreader.utils.FileUtils
 import de.thecode.android.tazreader.utils.StorageManager
 import de.thecode.android.tazreader.utils.UserDeviceInfo
 import timber.log.Timber
+import java.io.File
+
 
 class ErrorReporter {
     companion object {
@@ -36,11 +41,19 @@ class ErrorReporter {
                         .replaceFirst("{installid}", userDeviceInfo.installationId)
                         .replaceFirst("{aboid}", aboId)
                         .replaceFirst("{androidVersion}", Build.VERSION.SDK_INT.toString() + " (" + Build.VERSION.RELEASE + ")")
+                        .replaceFirst("{manufacturer}", Build.MANUFACTURER)
+                        .replaceFirst("{model}", Build.MODEL)
+                        .replaceFirst("{product}", Build.PRODUCT)
+                        .replaceFirst("{device}", Build.DEVICE)
+                        .replaceFirst("{brand}", Build.BRAND)
+                        .replaceFirst("{ram}", ramMemorySize(context))
+                        .replaceFirst("{archs}", userDeviceInfo.supportedArchList.joinToString(","))
+                        .replaceFirst("{space}", space(context))
                         .replaceFirst("{pushToken}", settings.firebaseToken)
 
                 emailIntent.putExtra(Intent.EXTRA_TEXT, body)
-            } catch (e : Exception) {
-                Timber.e(e,"Error parsing body")
+            } catch (e: Exception) {
+                Timber.e(e, "Error parsing body")
             }
             if (settings.isWriteLogfile) {
                 val logDir = StorageManager.getInstance(context)
@@ -68,6 +81,18 @@ class ErrorReporter {
                 settings.isWriteLogfile = false
             }
             context.startActivity(emailIntent)
+        }
+
+        private fun ramMemorySize(context: Context): String {
+            val mi = ActivityManager.MemoryInfo()
+            val activityManager = context.getSystemService(ACTIVITY_SERVICE) as ActivityManager?
+            activityManager!!.getMemoryInfo(mi)
+            return "${mi.availMem / 1048576L} (${mi.totalMem / 1048576L})"
+        }
+
+        private fun space(context: Context): String {
+            val currentPath = File(TazSettings.getInstance(context).dataFolderPath)
+            return "${currentPath.absolutePath} (frei: ${FileUtils.readableSize(currentPath.freeSpace)})"
         }
     }
 
